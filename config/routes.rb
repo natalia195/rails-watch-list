df8c8dd31374ca06fbb135676786e869ca90f023
natalia covarrubias <natalia.covarrubias@ug.uchile.cl>
2021-08-03 13:00:43 -0400

    avance por hoy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:index, :show]
+    resources :movies, only: [:index, :show]
+  end
+
+  resource :movies, only: [:show, :update, :destroy]
+
+  resolve("Basket") { route_for(:movies) }
 end
